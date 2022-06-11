2221f9b2c6020f7c87d76d919df417c556a88043
pawel <pjarkowski@edu.cdv.pl>
2022-06-11 18:04:32 +0200

    Fix a bug during app startup

--- app/crm/forms.py
+++ app/crm/forms.py
@@ -1,6 +1,24 @@
-from crm.models import Client, Files
 from django import forms
+from django.db import connection
 
+from crm.models import Client, Files
+all_tables = connection.introspection.table_names()
+
+def get_client_choices():
+    choices = [("all", "all")]
+    if "client" in all_tables:
+        if Client.objects.all().exists():
+            for client in Client.objects.all().values("name"):
+                choices.append((client["name"], client["name"]))
+    return choices
+
+def get_files_choices():
+    choices = []
+    if "crm_files" in all_tables:
+        if Files.objects.all().exists():
+            for file in Files.objects.all().values("uuid", "file_name"):
+                choices.append((str(file["uuid"]), file["file_name"]))
+    return choices
 
 class DateForm(forms.Form):
     start = forms.DateField(
@@ -11,25 +29,13 @@ class DateForm(forms.Form):
     )
 
 
-CHOICES = [
-    (client["name"], client["name"]) for client in Client.objects.all().values("name")
-]
-
-
 class ClientForm(forms.Form):
-    CHOICES.append(
-        ("all", "all"),
-    )
-    client = forms.ChoiceField(choices=sorted(CHOICES, key=lambda tup: tup[0]))
+    client = forms.ChoiceField(choices=sorted(get_client_choices(), key=lambda tup: tup[0]))
 
 
 class PDFForm(forms.Form):
-    FILES_CHOICE = [
-        (str(file["uuid"]), file["file_name"])
-        for file in Files.objects.all().values("file_name", "uuid")
-    ]
     files = forms.MultipleChoiceField(
-        choices=FILES_CHOICE, widget=forms.CheckboxSelectMultiple()
+        choices=get_files_choices(), widget=forms.CheckboxSelectMultiple()
     )
     send_to = forms.EmailField()
     message = forms.CharField()
