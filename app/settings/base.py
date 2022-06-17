a052cd16d1533024e7ce50bd596d22041c21339e
pawel jarkowski <pjarkowski@edu.cdv.pl>
2022-06-17 12:27:34 +0200

    Add req.txt

--- app/settings/base.py
+++ app/settings/base.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+import django_on_heroku
 from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ["DEBUG"] if os.environ.get("DEBUG") else True
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(
@@ -181,3 +182,5 @@ MESSAGE_TAGS = {
     messages.WARNING: "alert-warning",
     messages.ERROR: "alert-danger",
 }
+
+django_on_heroku.settings(locals())
